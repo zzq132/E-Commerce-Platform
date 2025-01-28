23131b93590264c88184686d8d23cb61f3023fdf
Zzz <winds2004318@163.com>
2025-01-28 15:19:42 +0800

    Finish the checkout part

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -74,10 +74,10 @@ const cartStore=useCartStore()
       <div class="action">
         <div class="batch">
           共 {{ cartStore.allCount }} 件商品，已选择 {{ cartStore.selectedNumber }} 件，商品合计：
-          <span class="red">¥ {{ cartStore.selectedPrice }} </span>
+          <span class="red">¥ {{ cartStore.selectedPrice.toFixed(2) }} </span>
         </div>
         <div class="total">
-          <el-button size="large" type="primary" >下单结算</el-button>
+          <el-button size="large" type="primary" @click="$router.push('/checkout')">下单结算</el-button>
         </div>
       </div>
     </div>
